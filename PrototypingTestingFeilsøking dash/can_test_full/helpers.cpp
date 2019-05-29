ed334c7eee240f81ae01b66f2f276b2eb3473514
ludvikronning <43721153+ludvikronning@users.noreply.github.com>
2019-05-29 20:39:18 +0200

    Add files via upload

---
+++ PrototypingTestingFeilsøking dash/can_test_full/helpers.cpp
@@ -0,0 +1,11 @@
+/* SEM 2019
+ * by Ludvik Rønning & Jørgen Wisløff
+ * 
+ */
+
+#include "helpers.h"
+
+void initSerial() {
+    // to communicate with the computer
+    Serial.begin(9600);
+}
---
+++ PrototypingTestingFeilsøking dash/test_can_bus/read_message/read_message.ino
@@ -0,0 +1,46 @@
+
+#include <FlexCAN.h>
+
+#ifndef __MK66FX1M0__
+  #error "Teensy 3.6 with dual CAN bus is required to run this example"
+#endif
+
+
+static CAN_message_t msg;
+static uint8_t hex[17] = "0123456789abcdef";
+
+
+// -------------------------------------------------------------
+static void hexDump(uint8_t dumpLen, uint8_t *bytePtr)
+{
+  uint8_t working;
+  while( dumpLen-- ) {
+    working = *bytePtr++;
+    Serial.write( hex[ working>>4 ] );
+    Serial.write( hex[ working&15 ] );
+  }
+  Serial.write('\r');
+  Serial.write('\n');
+}
+
+
+// -------------------------------------------------------------
+void setup(void)
+{
+  Can0.begin(500000);
+
+  delay(1000);
+  Serial.println(F("Hello Teensy 3.6 dual CAN Test."));
+}
+
+
+// -------------------------------------------------------------
+void loop(void)
+{
+  if(Can0.available()) 
+  {
+    Can0.read(msg);
+   Serial.print("CAN bus 0: "); hexDump(8, msg.buf);
+  }
+
+}
